f39280e905fefa92170575d7a7feebec4db9eca5
Abdi Abdiaziz <a.abdiaziz@hotmail.co.uk>
2018-10-01 09:21:48 +0100

    updated changes requested from code review

--- Database/Portfolio_2018-10-01.sql
+++ Database/Portfolio_2018-10-01.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.41)
 # Database: Portfolio
-# Generation Time: 2018-09-28 15:05:56 +0000
+# Generation Time: 2018-10-01 08:18:28 +0000
 # ************************************************************
 
 
@@ -20,29 +20,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-# Dump of table about_content
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `about_content`;
-
-CREATE TABLE `about_content` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title_of_content` varchar(30) NOT NULL DEFAULT '',
-  `content_` varchar(10000) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-LOCK TABLES `about_content` WRITE;
-/*!40000 ALTER TABLE `about_content` DISABLE KEYS */;
-
-INSERT INTO `about_content` (`id`, `title_of_content`, `content_`)
-VALUES
-	(1,'About Me','Hi, My name is Abdi. I have been Studying at university for 2 years previously, to joining Mayden Academy as a junior full stack developer. My growth during this course has sky rocketed');
-
-/*!40000 ALTER TABLE `about_content` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
 # Dump of table portfolio
 # ------------------------------------------------------------
 
@@ -52,22 +29,22 @@ CREATE TABLE `portfolio` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `project_title` varchar(11) DEFAULT NULL,
   `img_url` varchar(30) DEFAULT '',
-  `url_location` varchar(30) DEFAULT NULL,
-  `Deleted` enum('1','0') DEFAULT '0',
+  `url_location` varchar(1000) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `portfolio` WRITE;
 /*!40000 ALTER TABLE `portfolio` DISABLE KEYS */;
 
-INSERT INTO `portfolio` (`id`, `project_title`, `img_url`, `url_location`, `Deleted`)
+INSERT INTO `portfolio` (`id`, `project_title`, `img_url`, `url_location`, `deleted`)
 VALUES
-	(1,'Pilot shop','images/pilot.png','mini/pilotshop/pilot-shop.html','0'),
-	(2,'HTML + CSS','images/htmlcss.png','mini/html+css/helloworld.html','0'),
-	(4,'Project3','',NULL,'0'),
-	(5,'Project4','',NULL,'0'),
-	(6,'Project5','',NULL,'0'),
-	(7,'Project6','',NULL,'0');
+	(1,'Pilot shop','images/pilot.png','mini/pilotshop/pilot-shop.html',0),
+	(2,'HTML + CSS','images/htmlcss.png','mini/html+css/helloworld.html',0),
+	(4,'Project3','',NULL,0),
+	(5,'Project4','',NULL,0),
+	(6,'Project5','',NULL,0),
+	(7,'Project6','',NULL,0);
 
 /*!40000 ALTER TABLE `portfolio` ENABLE KEYS */;
 UNLOCK TABLES;
